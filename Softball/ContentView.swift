e9f00645a83af5044aab2adb35d38191a03840b4
Eric Forbes <ericscottforbes@gmail.com>
2024-12-06 17:27:49 -0500

    Initial Commit

---
+++ Softball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Softball
+//
+//  Created by Eric Forbes on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Softball/SoftballApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftballApp.swift
+//  Softball
+//
+//  Created by Eric Forbes on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SoftballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
